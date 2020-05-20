cb3d6de60cf2296f2616734217dc5e81967f394e
Hana Kontrec <hk5gc@gmail.com>
2020-05-20 11:03:01 -0400

    photos and cards working

--- src/mainpage.js
+++ src/mainpage.js
@@ -1,5 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
 import { makeStyles } from '@material-ui/core/styles'
 import {Card, 
         CardActionArea,
@@ -9,41 +8,46 @@ import {Card,
         Button, 
         Typography } from '@material-ui/core';
 
-
-=======
 import { importPets, getNewPet, reviewPet } from './store/actions.js';
->>>>>>> 57e6c393bf302fd8c125916dd2781ec357decfea
+
+const useStyles = makeStyles({
+    root: {
+      maxWidth: 345,
+    },
+    media: {
+      height: 140,
+    },
+  });
 
 export default function Main() {
-
+    const classes = useStyles();
     return (
         <>
             <h1>
                 This is our main page
             </h1>
             <div>
-                <Card>
+                <Card className={classes.root}> 
                 <CardActionArea>
                     <CardMedia
-                    image="/static/images/cards/contemplative-reptile.jpg"
-                    title="Contemplative Reptile"
+                    className={classes.media}
+                    image="https://images.dog.ceo/breeds/dingo/n02115641_4265.jpg"
                     />
                     <CardContent>
                     <Typography gutterBottom variant="h5" component="h2">
-                        Lizard
+                        Dog Name
                     </Typography>
                     <Typography variant="body2" color="textSecondary" component="p">
-                        Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-                        across all continents except Antarctica
+                        Breed: Dalmatian    
                     </Typography>
                     </CardContent>
                 </CardActionArea>
                 <CardActions>
                     <Button size="small" color="primary">
-                    Share
+                    Like
                     </Button>
                     <Button size="small" color="primary">
-                    Learn More
+                    Dislike 
                     </Button>
                 </CardActions>
                 </Card>
