5e80729eeac27e5e36f1235e3a5dba21f1115866
Hana Kontrec <hk5gc@gmail.com>
2020-05-20 10:47:34 -0400

    mainpage cards

--- src/mainpage.js
+++ src/mainpage.js
@@ -1,11 +1,49 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles'
+import {Card, 
+        CardActionArea,
+        CardContent, 
+        CardActions,
+        CardMedia, 
+        Button, 
+        Typography } from '@material-ui/core';
+
+
 
 export default function Main() {
+
     return (
         <>
             <h1>
                 This is our main page
             </h1>
+            <div>
+                <Card>
+                <CardActionArea>
+                    <CardMedia
+                    image="/static/images/cards/contemplative-reptile.jpg"
+                    title="Contemplative Reptile"
+                    />
+                    <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                        Lizard
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                        Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                        across all continents except Antarctica
+                    </Typography>
+                    </CardContent>
+                </CardActionArea>
+                <CardActions>
+                    <Button size="small" color="primary">
+                    Share
+                    </Button>
+                    <Button size="small" color="primary">
+                    Learn More
+                    </Button>
+                </CardActions>
+                </Card>
+           </div>
         </>
     )
 }
