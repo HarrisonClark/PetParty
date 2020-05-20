8137bcfc0d2b55f4b06672f441ddfdbe506f55d6
Hana Kontrec <hk5gc@gmail.com>
2020-05-20 10:15:45 -0400

    new changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React from 'react';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux'; 
+import Testing from './Testing'; 
+import reducer from './store/reducer'; 
+
+const store = createStore(reducer)
 
 function App() {
   return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store ={store}>
+      <Testing />
+    </Provider>
   );
 }
 
