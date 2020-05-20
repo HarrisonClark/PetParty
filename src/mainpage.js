bf8b60e0bf29901708458432a8ef41b32b5cdad5
Alex Stiles <alexanstiles@gmail.com>
2020-05-20 20:08:59 +0200

    made nice changes

--- src/mainpage.js
+++ src/mainpage.js
@@ -4,6 +4,7 @@ import { Box } from "@material-ui/core";
 import PetCard from "./PetCard";
 import { importPets, getNewPet, reviewPet } from "./store/actions.js";
 import Messaging from './Messaging';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 export default function Main() {
   return (
@@ -12,6 +13,8 @@ export default function Main() {
         <PetCard />
         <Messaging />
       </Box>
+      <div style={{marginBottom: 50}}/>
+      <CssBaseline />
     </>
   );
 }
