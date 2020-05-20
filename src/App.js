55b250ee64b860f01eb374ec1690eddae3cca2fc
Alex Stiles <alexanstiles@gmail.com>
2020-05-20 16:26:16 +0200

    added main page

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux'; 
 import Testing from './Testing'; 
 import reducer from './store/reducer'; 
+import Main from './mainpage';
 
 const store = createStore(reducer)
 
 function App() {
   return (
     <Provider store ={store}>
+      <Main />
       <Testing />
     </Provider>
   );
---
+++ src/mainpage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Main() {
+    return (
+        <>
+            <h1>
+                This is our main page
+            </h1>
+        </>
+    )
+}
