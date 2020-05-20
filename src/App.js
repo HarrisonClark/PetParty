9c511ff396fa7835198a7b46b48c8ac14462acef
Harrison Clark <harrison.clark99@icloud.com>
2020-05-20 10:18:53 -0500

    React router added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import React from 'react';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux'; 
-import Testing from './Testing'; 
-import reducer from './store/reducer'; 
-import Main from './mainpage';
+import React from "react";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import Testing from "./Testing";
+import reducer from "./store/reducer";
+import Main from "./mainpage";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-const store = createStore(reducer)
+const store = createStore(reducer);
 
-function App() {
+export default function App() {
   return (
-    <Provider store ={store}>
-      <Main />
-      <Testing />
-    </Provider>
+    <Router>
+      <Switch>
+        <Provider store={store}>
+          <Route exact path="/" component={Main} />
+          <Route exact path="/testing" component={Testing} />
+        </Provider>
+      </Switch>
+    </Router>
   );
 }
-
-export default App;
